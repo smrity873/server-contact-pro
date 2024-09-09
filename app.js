1ce8fbb751c0af0be6c5d2b47666f9a78cf6fccf
rhmunna143 <rhmunna19@gmail.com>
2024-09-09 21:55:04 +0600

    trying to fix red.body

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const { authenticateToken } = require('./middleware/authMiddleware');
 
 dotenv.config();
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 // Apply middleware
@@ -21,7 +23,6 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes);
 app.use('/contacts', authenticateToken, contactRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
